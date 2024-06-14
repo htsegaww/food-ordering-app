f4fa36faad8cec48e9e01fcb17ee6575b24bcdcd
Henok <hennok100@gmai.com>
2024-06-14 15:56:03 -0400

    changed

--- app/api/users/route.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "@/models/User";
-import mongoose from "mongoose";
-
-export async function GET() {
-  mongoose.connect(process.env.MONGO_URL!);
-  const users = await User.find();
-  return Response.json(users);
-}
