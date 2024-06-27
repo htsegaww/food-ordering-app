326779bf22cfebb4f7d57dc4931d09180e802745
Henok <hennok100@gmai.com>
2024-06-27 17:22:35 -0400

    env file updated

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -12,8 +12,8 @@ export async function POST(req: Request) {
     const s3Client = new S3Client({
       region: "us-east-2",
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY || "",
-        secretAccessKey: process.env.AWS_SECRET_KEY || "",
+        accessKeyId: process.env.MY_AWS_ACCESS_KEY || "",
+        secretAccessKey: process.env.MY_AWS_SECRET_KEY || "",
       },
     });
 
